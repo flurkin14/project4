39715c00a96c2ea29c5cfba1d25cff2dab2a6647
Marshall <106941159+flurkin14@users.noreply.github.com>
2022-07-21 17:04:27 +0000

    finished

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ import pygame, sys, time, random
 pygame.init()
 win=pygame.display.set_mode((500, 500))
 speed=5
-
+size=30
 yellow=(255, 255, 0)
 white=(255, 255, 255)
 red=(255, 0, 0)
 green=(0, 255, 0)
+black=(0,0,0)
 playerx=250
 playery=250
+
+poison=pygame.Rect(250,150,10,10)
+poison_status=True
+
+food=pygame.Rect(400,200,10,10)
+food_status=True
 
 while(True):
   pygame.time.delay(10)
@@ -27,15 +34,32 @@ while(True):
   if(keys[pygame.K_UP]):
     playery-=speed
 
-
-  
-
-  player=pygame.Rect(playerx, playery, 30,30)
-  pygame.draw.rect(win, red, player)
-  food=pygame.Rect(400,250,10,10)
-  pygame.draw.rect(win, green, food)
-  pygame.display.update()
+  player=pygame.Rect(playerx, playery, size,size)
 
 
   if player.colliderect(food):
     print("you are eating")
+    size+=5
+    food_status=False
+
+
+  if player.colliderect(poison):
+    print("you are eating")
+    size-=5
+    poison_status=False
+    
+  if (food_status==False):
+    food=pygame.Rect(random.randint(1,500),random.randint(1,500),10,10)
+    #food=True
+
+  if (poison_status==False):
+    poison=pygame.Rect(random.randint(1,500),random.randint(1,500),10,10)
+    
+  pygame.draw.rect(win, black, poison)
+    
+  pygame.draw.rect(win, red, player)
+  pygame.draw.rect(win, green, food)
+  pygame.display.update()
+
+
+  
